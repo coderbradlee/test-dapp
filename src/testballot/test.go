3ef3f9aa1d7615724c673d96c122daa91ad8d963
lzxm160 <lzxm160@sina.com>
2017-09-24 19:24:54 +0800

    test

--- src/testballot/test.go
+++ src/testballot/test.go
@@ -99,7 +99,7 @@ func simed() {
 	printProposals(ballot)
 	winnner,_:=ballot.WinnerName(nil)
 	fmt.Println(winnner)
-	testaddr,_:=ballot.Testmsg(chairmanAuth)
+	testaddr,_:=ballot.Testmsg(secondAddr)
 	fmt.Printf("testaddr addr:%v\n", testaddr.Hex())
 	testdata,_:=ballot.Testdata(nil)
 	fmt.Printf("testdata :%v\n", testdata)
